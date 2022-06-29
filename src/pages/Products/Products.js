913d6df73399fe3094bc90a053537a5b0eebeea3
Daniel Calle <danielcalleco@gmail.com>
2022-06-28 22:04:12 -0500

    first commit

---
+++ src/pages/Products/Products.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Products = () => {
+    return <div>Soy un producto</div>;
+};
+
+export default Products;
---
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShoppingCart = () => {
+    return <div>Soy el carrito</div>;
+};
+
+export default ShoppingCart;
