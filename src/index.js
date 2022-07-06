b85654f96e7f81c5afbc0e825932fbd124230e45
Daniel Calle <danielcalleco@gmail.com>
2022-07-05 22:58:37 -0500

    navbar made

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products";
 import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/organisms/Navbar/Navbar";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/Products" element={<Products />} />
